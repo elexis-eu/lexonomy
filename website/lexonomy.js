40a0240f5366ebcd959e0faab12031489c30fef4
Michal Boleslav Měchura <valselob@gmail.com>
2018-01-21 18:26:53 +0100

    names of override items in configs begin with underscore

--- website/lexonomy.js
+++ website/lexonomy.js
@@ -496,7 +496,7 @@ app.get(siteconfig.rootPath+":dictID/config/", function(req, res){
         ops.getDictStats(req.params.dictID, function(stats){
           res.render("config.ejs", {
             user: user, dictID: req.params.dictID, dictTitle: configs.ident.title, needResave: (stats.needResave>0), siteconfig: siteconfig,
-            hasXemaOverride: (configs.xema.xonomyDocSpec!=null),
+            hasXemaOverride: (configs.xema._xonomyDocSpec!=null),
             hasXemplateOverride: (configs.xemplate._xsl!=null || configs.xemplate._css!=null)
           });
         });
--- website/widgets/xema-override.js
+++ website/widgets/xema-override.js
@@ -11,7 +11,7 @@ XemaOverride.render=function(div, json){
   $block.find("select").on("change", function(e){XemaOverride.change();});
   $block.append("<div class='instro'>Bla bla...</div>");
 
-  var code=json.xonomyDocSpec;
+  var code=json._xonomyDocSpec;
   var $block=$("<div class='block theSchema'></div>").appendTo($div);
 	$block.append("<div class='title'>Document specification</div>");
   $block.append("<textarea class='textbox' spellcheck='false'></textarea>");
@@ -21,7 +21,7 @@ XemaOverride.render=function(div, json){
   $block.append("<div class='instro'>Bla bla...</div>");
   $block.append("<div class='error' style='display: none;'></div>");
 
-  var code=json.newXml;
+  var code=json._newXml;
   var $block=$("<div class='block newXml'></div>").appendTo($div);
 	$block.append("<div class='title'>Template for new entries</div>");
   $block.append("<textarea class='textbox' spellcheck='false'></textarea>");
@@ -33,18 +33,18 @@ XemaOverride.render=function(div, json){
 };
 XemaOverride.harvest=function(div){
   var ret={};
-  ret.xonomyDocSpec=$(".pillarform .block.theSchema textarea").val();
-  ret.newXml=$(".pillarform .block.newXml textarea").val();
+  ret._xonomyDocSpec=$(".pillarform .block.theSchema textarea").val();
+  ret._newXml=$(".pillarform .block.newXml textarea").val();
 
   //understand the docspec a little:
   ret.elements={};
-  var Xonomy={}; eval("var docspec="+ret.xonomyDocSpec+";");
+  var Xonomy={}; eval("var docspec="+ret._xonomyDocSpec+";");
   for(var elName in docspec.elements){
     ret.elements[elName]={};
   }
 
   //understand what the top-level element:
-  var match=ret.newXml.match(/^\<([^ \>\/]+)/);
+  var match=ret._newXml.match(/^\<([^ \>\/]+)/);
   if(match) ret.root=match[1];
   if(!ret.elements[ret.root]) for(var elName in ret.elements) {ret.root=elName; break;}
 
