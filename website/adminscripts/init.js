f12ff0083b5304e50afab1b181a8088e55ddffbf
egon w. stemle <egon.stemle@eurac.edu>
2019-04-25 18:38:27 +0200

    Use LEXONOMY_SITECONFIG for docker-compose
    Instead of mounting the file `./website/siteconfig.json.template` as
    `siteconfig.json` use the env var `LEXONOMY_SITECONFIG:
    siteconfig.json.template` from #123

--- website/adminscripts/init.js
+++ website/adminscripts/init.js
@@ -2,41 +2,28 @@ const path=require("path");
 const fs=require("fs-extra");
 const sqlite3 = require('sqlite3').verbose(); //https://www.npmjs.com/package/sqlite3
 const sha1 = require('sha1'); //https://www.npmjs.com/package/sha1
+const siteconfig = require("../siteconfig").load();
 
-fs.readFile(path.join(__dirname, "../siteconfig.json"), "utf8", function(err, content){
+var dbFile = path.join(siteconfig.dataDir, siteconfig.dbFile);
+
+// Prepare to initialise the DB
+var db = new sqlite3.Database(dbFile, function(err){
   if (err) {
-     console.log(err);
+      console.log(err)
+      return
   }
-  var siteconfig=JSON.parse(content);
-  var dbFile = path.join(siteconfig.dataDir, siteconfig.dbFile);
-
-  // Prepare to initialise the DB
-  var db = new sqlite3.Database(dbFile, function(err){
+  console.log('Connected to ' + dbFile + ' database.')
+});
+// Read the DB Schema into a String
+var dbSchema = fs.readFileSync(path.join(siteconfig.dataDir, siteconfig.dbSchemaFile), 'utf8');
+// Initialise the DB (in case it already exists this will print warninigs)
+db.exec(dbSchema, function(err){
     if (err) {
-        console.log(err)
-        return
+        console.log(err);
+        console.log("Likely the DB has already been created.");
+    } else {
+        console.log("Initialised " + siteconfig.dbFile + " with: \n" + dbSchema);
     }
-    console.log('Connected to ' + dbFile + ' database.')
-  });
-  // Read the DB Schema into a String
-  var dbSchema = fs.readFileSync(path.join(siteconfig.dataDir, siteconfig.dbSchemaFile), 'utf8');
-  // Initialise the DB (in case it already exists this will print warninigs)
-  db.exec(dbSchema, function(err){
-      if (err) {
-          console.log(err);
-          console.log("Likely the DB has already been created.");
-      } else {
-          console.log("Initialised " + siteconfig.dbFile + " with: \n" + dbSchema);
-      }
-  });
-
-  var password=Math.random().toString(36).slice(-10);
-  var passwordHash=sha1(password);
-  for(var i=0; i<siteconfig.admins.length; i++){
-    var email=siteconfig.admins[i];
-    insertUser(db, email, password, passwordHash);
-  }
-  db.close();
 });
 
 var insertUser=function(db, email, password, passwordHash){
@@ -51,3 +38,11 @@ var insertUser=function(db, email, password, passwordHash){
     }
   });
 };
+
+var password=Math.random().toString(36).slice(-10);
+var passwordHash=sha1(password);
+for(var i=0; i<siteconfig.admins.length; i++){
+  var email=siteconfig.admins[i];
+  insertUser(db, email, password, passwordHash);
+}
+db.close();
