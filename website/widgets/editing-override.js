3f42c72bf65c954280ac87c5ce60927d6046e48b
Milos Jakubicek <milos.jakubicek@sketchengine.co.uk>
2019-05-06 00:09:17 +0200

    use CodeMirror for the custom entry editor widget

--- website/widgets/editing-override.js
+++ website/widgets/editing-override.js
@@ -3,30 +3,47 @@ var EditingOverride={};
 
  EditingOverride.render=function(div, json){
   $(div).append("<div id='pagebody' class='notop'><div class='pillarform'></div></div>"); var $div=$(div).find("div.pillarform");
+  $div.append("<div class='instro'>You can toggle fullscreen view for each of the editing windows using F11/Esc (first click into the editing window).</div>");
+  var CodeMirrorOpts = {
+    matchBrackets: true,
+    lineNumbers: true,
+    styleActiveLine: true,
+    extraKeys: {
+      "F11": function(cm) {
+        cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+      },
+      "Esc": function(cm) {
+        if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+      }
+    }
+  }
 
-  var code=json._js;
+  CodeMirrorOpts.mode="javascript"
+  console.log(CodeMirrorOpts)
   var $block=$("<div class='block theJS'></div>").appendTo($div);
 	$block.append("<div class='title'><a href='javascript:void(null)' onclick=' EditingOverride.exampleJs()'>example</a> JavaScript</div>");
   $block.append("<textarea class='textbox' spellcheck='false'></textarea>");
-  $block.find("textarea").val(code).data("origval", code).on("change keyup", function(e){
-    if($div.find(".block.theJS textarea").val()!=$div.find(".block.theJS textarea").data("origval"))  EditingOverride.change();
-  });
-  //$block.append("<div class='instro'>Bla bla...</div>");
+  $block.append("<div class='instro'>You can customize the editor by defining two functions in JavaScript: one that will render the HTML editor from the XML entry and one that will harvest the (edited) HTML back into XML. Click on the example link in the upper right corner to load a sample code.</div>");
   $block.append("<div class='error' style='display: none;'></div>");
+  CodeMirrorJS = CodeMirror.fromTextArea($block.find("textarea")[0], CodeMirrorOpts);
+  if (json._js)
+    CodeMirrorJS.setValue(json._js);
+  CodeMirrorJS.on("change", EditingOverride.change);
 
-  var code=json._css;
+  CodeMirrorOpts.mode="css"
   var $block=$("<div class='block theCSS'></div>").appendTo($div);
 	$block.append("<div class='title'><a href='javascript:void(null)' onclick=' EditingOverride.exampleCss()'>example</a> CSS</div>");
   $block.append("<textarea class='textbox' spellcheck='false'></textarea>");
-  $block.find("textarea").val(code).data("origval", code).on("change keyup", function(e){
-    if($div.find(".block.theCSS textarea").val()!=$div.find(".block.theCSS textarea").data("origval"))  EditingOverride.change();
-  });
-  //$block.append("<div class='instro'>Bla bla...</div>");
+  $block.append("<div class='instro'>You can customize the editor look and feel by writing your own CSS styles. Click on the example link in the upper right corner to load a sample sheet.</div>");
   $block.append("<div class='error' style='display: none;'></div>");
+  CodeMirrorCSS = CodeMirror.fromTextArea($block.find("textarea")[0], CodeMirrorOpts);
+  if (json._css)
+    CodeMirrorCSS.setValue(json._css);
+  CodeMirrorCSS.on("change", EditingOverride.change);
 };
 
  EditingOverride.exampleJs=function(){
-  $(".pillarform .block.theJS textarea").val(
+  CodeMirrorJS.setValue(
 `{
   editor: function(div, entry, uneditable){
     //div = the div into which you should render the editor
@@ -63,7 +80,7 @@ var EditingOverride={};
   );
 };
  EditingOverride.exampleCss=function(){
-  $(".pillarform .block.theCSS textarea").val(
+  CodeMirrorCSS.setValue(
 `div.myEditor {padding: 20px; font-size: 1.25em}
 div.myEditor input {font-weight: bold}`
   );
@@ -71,7 +88,7 @@ div.myEditor input {font-weight: bold}`
 
  EditingOverride.harvest=function(div){
   var ret={};
-  ret._js=$(".pillarform .block.theJS textarea").val();
-  ret._css=$(".pillarform .block.theCSS textarea").val();
+  ret._js=CodeMirrorJS.getValue();
+  ret._css=CodeMirrorCSS.getValue();
   return ret;
 };
