fd14be0244698a6bf40aefaa4a68ec976b24eb42
Milos Jakubicek <milos.jakubicek@sketchengine.co.uk>
2019-11-04 17:35:34 +0100

    fix some English and add more info on download XSLT

--- website/widgets/download.js
+++ website/widgets/download.js
@@ -9,10 +9,10 @@ Download.ifchange=function(event){
 
 Download.render=function(div, json){
   $(div).append("<div id='pagebody' class='notop'><div class='pillarform'></div></div>"); var $div=$(div).find("div.pillarform");
-  $div.append("<div class='title'>Download xslt script</div>");
+  $div.append("<div class='title'>XSLT transformation on download</div>");
   $div.append("<textarea class='textbox' id='download_xslt' spellcheck='false'></textarea>");
   $div.find("#download_xslt").val(json.xslt).data("origval", json.xslt).on("change keyup", Download.ifchange);
-  $div.append("<div class='instro'>If input is not a valid xslt, no transform is applied.</div>");
+  $div.append("<div class='instro'>You can use this functionality to automatically apply an XSLT transformation when the dictionary is downloaded. If you do not input valid XSLT here, no transformation will be applied.</div>");
 };
 
 Download.harvest=function(div){
