bdb5ad09e46aee90cca4b80d7392b538d5656c68
Adam Rambousek <rambousek@gmail.com>
2020-10-20 18:05:22 +0200

    Fix searching for subentries

--- website/lexonomy.py
+++ website/lexonomy.py
@@ -210,7 +210,7 @@ def entryflag(dictID, user, dictDB, configs):
 @get(siteconfig["rootPath"]+"<dictID>/subget")
 @authDict(["canEdit"])
 def subget(dictID, user, dictDB, configs):
-    total, entries = ops.listEntries(dictDB, dictID, configs, request.query.doctype, request.query.lemma, "wordstart", 100, False, True)
+    total, entries = ops.listEntries(dictDB, dictID, configs, request.query.doctype, request.query.lemma, "wordstart", 100, False, False, True)
     return {"success": True, "total": total, "entries": entries}
 
 @post(siteconfig["rootPath"]+"<dictID>/history.json")
