d0c9c9fa4b107f66e576d20412c4772ceada469c
Adam Rambousek <rambousek@gmail.com>
2021-03-17 17:03:10 +0100

    Parse different output locale.

--- website/adminscripts/linkNAISC.sh
+++ website/adminscripts/linkNAISC.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -ne 5 ]; then
   echo "Usage: $0 DATADIR DB1 DB2 NAISCCMD JOBID"
@@ -11,8 +11,8 @@ DB2="$3"
 NAISCCMD="$4"
 JOBID="$5"
 CURRDIR=`dirname $0`
-$CURRDIR/xml2ontolex.py $DATADIR/dicts/$DB1.sqlite > /tmp/linkNAISC-$DB1.nt
-$CURRDIR/xml2ontolex.py $DATADIR/dicts/$DB2.sqlite > /tmp/linkNAISC-$DB2.nt
+python3 $CURRDIR/xml2ontolex.py $DATADIR/dicts/$DB1.sqlite > /tmp/linkNAISC-$DB1.nt
+python3 $CURRDIR/xml2ontolex.py $DATADIR/dicts/$DB2.sqlite > /tmp/linkNAISC-$DB2.nt
 $NAISCCMD /tmp/linkNAISC-$DB1.nt /tmp/linkNAISC-$DB2.nt -c configs/auto.json -o /tmp/linkNAISC-$DB1-$DB2
 RET=$?
 [ $RET -eq 0 ] && $CURRDIR/importNAISClinks.py $DATADIR/crossref.sqlite < /tmp/linkNAISC-$DB1-$DB2
