e251b6d995f1627ab42ac49ca00598eb659bfc6b
Adam Rambousek <rambousek@gmail.com>
2021-06-01 17:53:50 +0200

    Make xref link working

--- website/widgets/xemplatron.js
+++ website/widgets/xemplatron.js
@@ -132,7 +132,7 @@ Xemplatron.styles={ //the dimensions are ordered from innermost to outermost
   },
   interactivity: {
     "title": "Interactivity",
-    "xref": {toHtml: function(ly, html, n, isF, isL){return "<a class='xref' href='#' data-text='"+n.textContent.replace("'", "&apos;")+"'>"+html+"</a>"}, title: "Clickable cross-reference"},
+    "xref": {toHtml: function(ly, html, n, isF, isL){return "<a class='xref' onclick='route(\""+dictId+(document.location.hash.includes("/edit/")?"/edit":"")+"/"+n.nodeName+"/view"+(n.getAttribute('lxnm:subentryID'))+"\");' data-text='"+n.textContent.replace("'", "&apos;")+"'>"+html+"</a>"}, title: "Clickable cross-reference"},
   },
   innerPunc: {
     "title": "Inner punctuation",
