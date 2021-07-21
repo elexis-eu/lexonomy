a72df7f009324a90fa33662b49833af08636018d
Adam Rambousek <rambousek@gmail.com>
2021-07-21 17:21:21 +0200

    Image search, add Wikidata

--- website/media.py
+++ website/media.py
@@ -7,6 +7,7 @@ def get_images(configs, query):
         configs["gapi"]["image_licence"] = "code"
     if configs["gapi"] and configs["gapi"]["pixabaykey"] != "":
         images.extend(get_image_pixabay(query, configs["gapi"]["pixabaykey"]))
+    images.extend(get_image_wikidata(query, configs["gapi"]["image_licence"]))
     if configs["gapi"] and configs["gapi"]["apikey"] != "" and configs["gapi"]["cx"] != "":
         images.extend(get_image_google(query, configs["gapi"]["apikey"], configs["gapi"]["cx"], configs["gapi"]["image_licence"]))
     return images
@@ -51,8 +52,60 @@ def get_image_google(query, apikey, cx, licence):
     results = requests.get(endpoint, params=params)
     for item in results.json()['items']:
         images.append({
-            'title': item['title'],
+            'title': 'Wikimedia: ' + item['title'],
             'thumb': item['image']['thumbnailLink'],
             'url': item['link']
         })
     return images
+
+def get_image_wikidata(query, licence):
+    images = []
+    endpoint = 'https://www.wikidata.org/w/api.php'
+    params = {
+        'action': 'wbsearchentities',
+        'search': query,
+        'language': 'en',
+        'format': 'json',
+        'limit': 20
+    }
+    results = requests.get(endpoint, params=params)
+    for item in results.json()['search']:
+        for file in get_wikidata_file_name(item['id']):
+            for info in get_wikidata_info(file).values():
+                if licence == 'public' and info['imageinfo'][0]['extmetadata']['LicenseShortName']['value'] != 'Public':
+                    next
+                if licence in ['comm', 'der', 'cod'] and "CC BY-SA" not in info['imageinfo'][0]['extmetadata']['LicenseShortName']['value']:
+                    next
+                images.append({
+                    'title': info['title'],
+                    'thumb': info['imageinfo'][0]['url'],
+                    'url': info['imageinfo'][0]['url']
+                })
+    return images
+
+def get_wikidata_file_name(source_id):
+    endpoint = 'https://www.wikidata.org/w/api.php'
+    params = {
+        'action': 'wbgetclaims',
+        'entity': source_id,
+        'property': 'P18',
+        'format': 'json',
+    }
+    results = requests.get(endpoint, params=params).json()
+    try:
+        return [x['mainsnak']['datavalue']['value'] for x in results['claims']['P18']]
+    except KeyError:
+        return []
+
+def get_wikidata_info(file_name):
+    endpoint = 'https://commons.wikimedia.org/w/api.php'
+    params = {
+        'action': 'query',
+        'titles': 'File:{}'.format(file_name),
+        'prop': 'imageinfo',
+        'format': 'json',
+        'iiprop': 'url|user|extmetadata|dimensions'
+    }
+    results = requests.get(endpoint, params=params).json()
+    return results['query']['pages']
+
