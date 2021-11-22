5bfdce7efa392667bf64e4f7377de6fe99af2449
Adam Rambousek <rambousek@gmail.com>
2021-11-22 18:41:33 +0100

    Link API, support for concept dictionary on both sides

--- website/ops.py
+++ website/ops.py
@@ -685,7 +685,7 @@ def getLinkList(headword, sourceLang, sourceDict, targetLang):
                             lia = r2["source_id"].split("_")
                             info["targetSense"] = lia[1]
                         query3 = "SELECT DISTINCT l.entry_id AS entry_id, l.txt AS link_id, l.element AS link_el, s.txt AS hw FROM searchables AS s, linkables AS l  WHERE s.entry_id=l.entry_id AND l.txt=? AND s.level=1"
-                        c3 = getDB(r2["source_dict"]).execute(query3, (r2["source_id"],))
+                        c3 = sourceDB.execute(query3, (r2["source_id"],))
                         for r3 in c3.fetchall():
                             info["targetHeadword"] = r3["hw"]
                             info["targetID"] = r3["entry_id"]
@@ -699,6 +699,79 @@ def getLinkList(headword, sourceLang, sourceDict, targetLang):
                         info["targetSense"] = ""
                         info["targetLang"] = ""
                         links.append(info)
+        else:
+            # source dictionary is "concept", use headword as target_id
+            info0 = {"sourceDict": d["id"], "sourceHeadword": headword, "sourceID": headword, "sourceDictConcept": True, "sourceURL": "", "sourceSense": ""}
+            # first, find links with searched dict as source
+            if targetLang:
+                targetDicts = []
+                for td in getDictList(targetLang, True):
+                    targetDicts.append(td["id"])
+                query2 = "SELECT * FROM links WHERE source_dict=? AND source_id=? AND target_dict IN "+"('"+"','".join(targetDicts)+"')"
+            else:
+                query2 = "SELECT * FROM links WHERE source_dict=? AND source_id=?"
+            data2 = (d["id"], headword)
+            c2 = linkDB.execute(query2, data2)
+            for r2 in c2.fetchall():
+                info = info0.copy()
+                info["targetDict"] = r2["target_dict"]
+                info["confidence"] = r2["confidence"]
+                targetDB = getDB(r2["target_dict"])
+                if targetDB:
+                    info["targetLang"] = readDictConfigs(targetDB)['ident']['lang']
+                    info["targetDictConcept"] = False
+                    if r2["target_element"] == "sense" and "_" in r2["target_id"]:
+                        lia = r2["target_id"].split("_")
+                        info["targetSense"] = lia[1]
+                    query3 = "SELECT DISTINCT l.entry_id AS entry_id, l.txt AS link_id, l.element AS link_el, s.txt AS hw FROM searchables AS s, linkables AS l  WHERE s.entry_id=l.entry_id AND l.txt=? AND s.level=1"
+                    c3 = targetDB.execute(query3, (r2["target_id"],))
+                    for r3 in c3.fetchall():
+                        info["targetHeadword"] = r3["hw"]
+                        info["targetID"] = r3["entry_id"]
+                        info["targetURL"] = siteconfig["baseUrl"] + info["targetDict"] + "/" + str(info["targetID"])
+                        links.append(info)
+                else:
+                    info["targetHeadword"] = r2["target_id"]
+                    info["targetID"] = r2["target_id"]
+                    info["targetDictConcept"] = True
+                    info["targetURL"] = ""
+                    info["targetSense"] = ""
+                    info["targetLang"] = ""
+                    links.append(info)
+            # second, find links with search dict as target
+            if targetLang:
+                query2 = "SELECT * FROM links WHERE target_dict=? AND target_id=? AND source_dict IN "+"('"+"','".join(targetDicts)+"')"
+            else:
+                query2 = "SELECT * FROM links WHERE target_dict=? AND target_id=?"
+            data2 = (d["id"], headword)
+            c2 = linkDB.execute(query2, data2)
+            for r2 in c2.fetchall():
+                info = info0.copy()
+                info["targetDict"] = r2["source_dict"]
+                info["confidence"] = r2["confidence"]
+                sourceDB = getDB(r2["source_dict"])
+                if sourceDB:
+                    info["targetLang"] = readDictConfigs(sourceDB)['ident']['lang']
+                    info["targetDictConcept"] = False
+                    if r2["source_element"] == "sense" and "_" in r2["source_id"]:
+                        lia = r2["source_id"].split("_")
+                        info["targetSense"] = lia[1]
+                    query3 = "SELECT DISTINCT l.entry_id AS entry_id, l.txt AS link_id, l.element AS link_el, s.txt AS hw FROM searchables AS s, linkables AS l  WHERE s.entry_id=l.entry_id AND l.txt=? AND s.level=1"
+                    c3 = sourceDB.execute(query3, (r2["source_id"],))
+                    for r3 in c3.fetchall():
+                        info["targetHeadword"] = r3["hw"]
+                        info["targetID"] = r3["entry_id"]
+                        info["targetURL"] = siteconfig["baseUrl"] + info["targetDict"] + "/" + str(info["targetID"])
+                        links.append(info)
+                else:
+                    info["targetHeadword"] = r2["source_id"]
+                    info["targetID"] = r2["source_id"]
+                    info["targetDictConcept"] = True
+                    info["targetURL"] = ""
+                    info["targetSense"] = ""
+                    info["targetLang"] = ""
+                    links.append(info)
+
     return links
 
 def listUsers(searchtext, howmany):
