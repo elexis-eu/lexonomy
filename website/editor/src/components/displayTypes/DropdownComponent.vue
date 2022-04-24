b341f60b7ac69fe54fe3b007f5e03b33b8a58b02
Matic Tersek <matic.diamond@gmail.com>
2022-04-24 14:26:03 +0200

    Dropdown component somewhat funcitonal

--- website/editor/src/components/displayTypes/DropdownComponent.vue
+++ website/editor/src/components/displayTypes/DropdownComponent.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <section v-if="elementData.shown">
-<!--      <p>dropdown component</p>-->
+    <section v-if="elementData.shown" class="dropdown">
       <p> {{ elementName }}</p>
+      <select v-model="value">
+        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.caption }}</option>
+      </select>
     </section>
   </div>
 </template>
@@ -18,10 +20,33 @@ export default {
       type: Object,
       required: true
     }
+  },
+  data() {
+    return {
+      value: null,
+      options: []
+    }
+  },
+  created() {
+    let structureConfig = this.state.entry.dictConfigs.xema.elements[this.elementName]
+    this.options = structureConfig.values
+    this.value = this.content._text
   }
 }
 </script>
 
 <style scoped>
+.dropdown {
+  display: flex;
+  justify-content: center;
+}
 
+.dropdown p {
+  margin-right: 8px;
+}
+
+select {
+  display: block;
+  width: auto;
+}
 </style>
