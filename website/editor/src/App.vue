171f30195fa02233afa4facf742d1da5feb4fa2a
Matic Tersek <matic.diamond@gmail.com>
2022-04-27 17:12:08 +0200

    Change data passing to editor + move editor to dict-edit

--- website/editor/src/App.vue
+++ website/editor/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="editor">
     <div>
       <h4>EntryID</h4>
       <p>{{ state.entry.entryId }}</p>
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import axios from "axios"
 import {xml2js} from "xml-js"
 import EntryDisplay from "@/components/EntryDisplay"
 
@@ -19,78 +18,28 @@ export default {
   props: {
     message: String
   },
-  // watch: {
-  //   // "state.entry": {
-  //   //   handler(newVal) {
-  //   //     // console.log(newVal)
-  //   //   },
-  //   //   deep: true
-  //   // },
-  //   "state.entry.entryId"(newVal) {
-  //     this.readEntry(newVal)
-  //   }
-  // },
-  computed: {
-    entriesToRender() {
-      console.log(this.state.entry["entryId"], "inside compuited")
-      return this.state.entry.content
-    }
-  },
   created() {
-    this.bus.$on("updateEntry", this.updateEntry)
-    window.postMessage(
-      {
-        event: "editorReady",
-        data: {
-          ready: true
-        }
-      },
-      "*"
-    )
-  },
-  beforeDestroy() {
-    this.bus.$off("updateEntry", this.updateEntry)
+    let data = window.entryData
+    this.updateEntry(data)
   },
   methods: {
-    async updateEntry(data) {
+    updateEntry(data) {
       if(!data) {
         return
       }
       if(data.entryId !== "" && data.entryId !== this.state.entry.entryId) {
-        let contentData = {content: {}, contentHtml: ""}
-        contentData = await this.readEntry(data.entryId, data.dictId)
-        this.state.entry = {...this.state.entry, ...data, ...contentData}
+        data.content = xml2js(data.content || "", {compact: true})
+        this.state.entry = {...this.state.entry, ...data}
       } else {
         console.log("entry same or invalid")
       }
-    },
-    readEntry(id, dictId) {
-      // console.log("entryId", id)
-      let data = new FormData()
-      data.append("id", id)
-      return axios.post(`/${dictId}/entryread.json`, data)
-        .then(response => {
-          if(response.data.success) {
-
-            let content = xml2js(response.data.content, {compact: true})
-            let contentHtml = response.data.contentHtml
-            return {content: content, contentHtml: contentHtml}
-          } else {
-            console.error("Failed to load")
-            return {content: {}, contentHtml: ""}
-          }
-        })
-        .catch(e => {
-          console.error(e.message)
-          return {content: {}, contentHtml: ""}
-        })
     }
   }
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+#editor {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
