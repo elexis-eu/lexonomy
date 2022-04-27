0517c34cd45c9a46958f82863482bb71568d4322
Tomáš Svoboda <tomas.svoboda@sketchengine.co.uk>
2022-04-27 17:31:15 +0200

    added sass preprocessor
    (cherry picked from commit 9e3f01abe5143421f635bd33f4bd35caed6e3287)

--- website/rollup.config.js
+++ website/rollup.config.js
@@ -1,5 +1,19 @@
 import riot from 'rollup-plugin-riot'
 import nodeResolve from '@rollup/plugin-node-resolve'
+import { registerPreprocessor } from '@riotjs/compiler'
+import sass from 'node-sass'
+
+registerPreprocessor('css', 'scss', function(code, { options }) {
+  const { file } = options
+  const {css} = sass.renderSync({
+    data: code
+  })
+  return {
+    code: css.toString(),
+    map: null
+  }
+})
+
 const options = {}
 export default {
   input: 'app.js',
