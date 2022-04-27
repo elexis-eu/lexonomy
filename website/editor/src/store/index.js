eefb7e02261efdd5f33d4980a2d6432d2c0d4cee
Matic Tersek <matic.diamond@gmail.com>
2022-04-27 17:07:55 +0200

    Remove unused

--- website/editor/src/store/index.js
+++ website/editor/src/store/index.js
@@ -2,12 +2,10 @@
 var state = {
   entry: {
     entryId: null,
-    mode: null,
     dictId: null,
     dictConfigs: null,
     userAccess: null,
     userInfo: null,
-    userDicts: null,
     editorConfig: null,
     content: {},
     contentHtml: null
