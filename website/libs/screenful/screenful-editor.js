9a5d2873794d642c9a9af7a5fbe635ef1d5d91c9
Adam Rambousek <rambousek@gmail.com>
2022-05-13 14:15:55 +0200

    Links, display link preview text

--- website/libs/screenful/screenful-editor.js
+++ website/libs/screenful/screenful-editor.js
@@ -269,12 +269,14 @@ Screenful.Editor={
         linkElement.append('<span id="outlinks"><h2>Outgoing links</h2></span>');
         for (var link in links.out) {
           var linkdata = links.out[link];
+          var preview = linkdata["source_id"];
+          if (linkdata["source_preview"] != "") preview = linkdata["source_preview"];
           if (linkdata['target_hw'] != '') {
-            var linkhtml = '<ul>'+linkdata["source_id"]+' → <a target="_top" href="/'+linkdata['target_dict']+'/edit/entry/view'+linkdata['target_entry']+'">'+linkdata['target_dict']+' : '+linkdata['target_hw']+' : '+linkdata['target_el']+' : '+linkdata['target_id']+'</a>';
+            var linkhtml = '<ul>'+preview+' → <a target="_top" href="/'+linkdata['target_dict']+'/edit/entry/view'+linkdata['target_entry']+'">'+linkdata['target_dict']+' : '+linkdata['target_el']+' : '+linkdata['target_id']+'</a>';
           } else if (linkdata['target_entry'] != '') {
-            var linkhtml = '<ul>'+linkdata["source_id"]+' → <a target="_top" href="/'+linkdata['target_dict']+'/edit/entry/view'+linkdata['target_entry']+'">'+linkdata['target_dict']+' : '+linkdata['target_el']+' : '+linkdata['target_id']+'</a>';
+            var linkhtml = '<ul>'+preview+' → <a target="_top" href="/'+linkdata['target_dict']+'/edit/entry/view'+linkdata['target_entry']+'">'+linkdata['target_dict']+' : '+linkdata['target_el']+' : '+linkdata['target_id']+'</a>';
           } else {
-            var linkhtml = '<ul>'+linkdata["source_id"]+' → <a target="_top" href="/'+linkdata['target_dict']+'">'+linkdata['target_dict']+'</a> : '+linkdata['target_el']+' : '+linkdata['target_id'];
+            var linkhtml = '<ul>'+preview+' → <a target="_top" href="/'+linkdata['target_dict']+'">'+linkdata['target_dict']+'</a> : '+linkdata['target_el']+' : '+linkdata['target_id'];
           }
           if (linkdata['confidence'] && linkdata['confidence'] != '') {
             linkhtml += ' ('+linkdata['confidence']+')';
