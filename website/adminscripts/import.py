19b93c3c2427bee368645eced00d0ae72b92a3db
KCMertens <koen.mmm@gmail.com>
2022-05-26 18:26:43 +0300

    Fix after rebase

--- website/adminscripts/import.py
+++ website/adminscripts/import.py
@@ -8,6 +8,8 @@ import datetime
 import json
 import xml.sax
 import re
+import argparse
+from xml.parsers.expat import ParserCreate
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import ops
 
@@ -26,33 +28,17 @@ parser.add_argument("database", help="Path to dictionary.sqlite, this should alr
 parser.add_argument("file", help="Path to file_to_import.xml")
 args = parser.parse_args()
 
-if len(sys.argv) < 3:
-    print("Usage: ./import.py [-p] PATH_TO_DICTIONARY.sqlite FILE_TO_IMPORT.xml [AUTHOR_EMAIL]")
-    print("       -p   purge dictionary before importing but keep backup in history")
-    print("       -pp  like -p but do not keep purged content in history and purge any existing history as well")
-    sys.exit()
 
 print("PID "+ str(os.getpid()))
-print("Import started. You may close the window, import will run in the background. Please wait...")
-args = sys.argv[1:]
-purge = False
-purge_history = False
-if args[0] == "-p":
-    purge = True
-    args.pop(0)
-elif args[0] == "-pp":
-    purge = True
-    purge_history = True
-    args.pop(0)
+print("Import started. Please wait...")
 
-
-dbname = args[0]
-filename = args[1]
-email = args[2] if len(args)>2 else "IMPORT@LEXONOMY"
-dictID = os.path.basename(dbname).replace(".sqlite", "")
-db = sqlite3.connect(dbname)
+dictID = os.path.basename(args.database).replace(".sqlite", "")
+db = sqlite3.connect(args.database, )
 db.row_factory = sqlite3.Row
-historiography={"importStart": str(datetime.datetime.utcnow()), "filename": os.path.basename(filename)}
+db.isolation_level=None
+db.execute('PRAGMA synchronous = 0')
+db.execute("PRAGMA cache_size = -100000") # about 100 megs
+db.execute("begin")
 
 historiography={"importStart": str(datetime.datetime.utcnow()), "filename": os.path.basename(args.file)}
 
@@ -71,8 +57,6 @@ if args.purge or args.purge_history:
     db.execute("delete from linkables")
     db.execute("delete from sub")
     db.execute("delete from entries")
-    db.execute("delete from linkables")
-    db.execute("delete from searchables")
     db.commit()
     print("Compressing database...")
     db.execute("vacuum")
@@ -87,8 +71,8 @@ entryTag: str = xema.get("root", "")
 rootTag: str = "" # unused apart from entry tag detection code
 xmldata: bytes = open(args.file, 'rb').read() # NOTE: do not decode into string, otherwise we cannot extract the entries because byte and char offsets will mismatch. What I think happens is  sax returns byte offsets, while if we decode into string here, slices on this string will interpret indices as char offsets.
 
-# second pass, we know what the entry is and can import that
-import re
+if entryTag:
+    print("Entry tag from xema: " + entryTag)
 
 # If there is no document structure defined (the xema is empty?) 
 # Then assume the dictionary looks like this, so detect the first child of the root, and set that as entryTag
@@ -119,29 +103,14 @@ if not entryTag:
     xmldata = re.sub('<\?xml[^?]*\?>'.encode('utf-8'), b'', xmldata)
     xmldata = re.sub('<!DOCTYPE[^>]*>'.encode('utf-8'), b'', xmldata)
     try:
-        xml.sax.parseString(entry, xml.sax.handler.ContentHandler())
+        saxParser = xml.sax.parseString(xmldata, handlerFirst())
     except xml.sax._exceptions.SAXParseException as e:
-        skip = True
-        print("Skipping entry, XML parsing error: " + str(e), file=sys.stderr)
-        print("Skipping entry, XML parsing error: " + str(e))
-        print("Entry with error: " + entry, file=sys.stderr)
-        entryInserted += 1
-    if not skip:
-        pat = r'^<[^>]*\s+lxnm:(sub)?entryID=[\'"]([0-9]+)["\']'
-        entryID = None
-        action = "create"
-        title = ops.getEntryTitle(entry, configs["titling"])
-        sortkey = ops.getSortTitle(entry, configs["titling"])
-        if re.match(pat, entry):
-            entryID = re.match(pat, entry).group(2)
-            c = db.execute("select id from entries where id=?", (entryID,))
-            if not c.fetchone():
-                sql = "insert into entries(id, xml, needs_refac, needs_resave, needs_refresh, doctype, title, sortkey) values(?, ?, ?, ?, ?, ?, ?, ?)"
-                params = (entryID, entry, needs_refac, needs_resave, 0, entryTag, title, sortkey)
-            else:
-                sql = "update entries set doctype=?, xml=?, needs_refac=?, needs_resave=?, needs_refresh=? , title=?, sortkey=? where id=?"
-                params = (entryTag, entry, needs_refac, needs_resave, 0, title, sortkey, entryID)
-                action = "update"
+        if "junk after document element" in str(e):
+            xmldata = "<fakeroot>"+xmldata+"</fakeroot>"
+            rootTag = ""
+            entryTag = ""
+            entryCount = 0
+            saxParser = xml.sax.parseString(xmldata, handlerFirst()) # if this errors too, just throw
         else:
             if entryTag == "":
                 print("Not possible to detect element name for entry, please fix errors:")
