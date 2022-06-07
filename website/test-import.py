elexis-eu/lexonomy

commit 0bf9f92f45f9740d82a4b1eb0f6d745f52dc091e

legacy interop: use ID in legacy attribute "lxnm:entryID" if no "lxnm:id"

website/ops.py
@@ -709,13 +709,16 @@ def get_entry_id(xml: Tag, dictDB: Connection, maybeID: Optional[int] = None) ->
         xml.attrs["lxnm:id"] = str(maybeID)
         id = None
     
-    id = xml.attrs.get('lxnm:id') # check if xml has an id, if not, create one.
-    if id != None:
+    id = xml.attrs.get('lxnm:id') # check if xml has an id, if not, create one. (entryID is legacy)
+    if id is None:
+        id = xml.attrs.get('lxnm:entryID') # id used to be stored in lxnm:entryID once upon a time: legacy support
+    if id is not None:
         try: 
             id = int(id)
         except:
             print(f"Invalid ID (non-number) {id} in to-be-imported entry: replacing...")
             del xml.attrs["lxnm:id"]
+            del xml.attrs["lxnm:entryID"]
             id = None
 
     isNewEntry: bool = False
@@ -727,6 +730,7 @@ def get_entry_id(xml: Tag, dictDB: Connection, maybeID: Optional[int] = None) ->
         isNewEntry = doSql(dictDB, "insert into entries(id, doctype, xml, title, sortkey) values(?,?,?,?,?) on conflict(id) do nothing", (id, "", "", "", "")).rowcount > 0
 
     xml.attrs["lxnm:id"] = str(id)
+    del xml.attrs["lxnm:entryID"] # delete legacy ID
     return id, isNewEntry
 
 def presave_subentries(dictDB: Connection, configs: Configs, entryXml: Tag, entryID: int, email: str):

commit d7fed9a42250051aa22dab969f62ed01eb985a83

Fix bug introduced in 0bf9f92f45f9740d82a4b1eb0f6d745f52dc091e
- fix  error thrown when trying to delete nonexistant attribute.
- add a test validating the behavior

website/ops.py
@@ -717,8 +717,10 @@ def get_entry_id(xml: Tag, dictDB: Connection, maybeID: Optional[int] = None) ->
             id = int(id)
         except:
             print(f"Invalid ID (non-number) {id} in to-be-imported entry: replacing...")
-            del xml.attrs["lxnm:id"]
-            del xml.attrs["lxnm:entryID"]
+            if "lxnm:id" in xml.attrs:
+                del xml.attrs["lxnm:id"]
+            if "lxnm:entryID" in xml.attrs:
+                del xml.attrs["lxnm:entryID"]
             id = None
 
     isNewEntry: bool = False
@@ -730,7 +732,8 @@ def get_entry_id(xml: Tag, dictDB: Connection, maybeID: Optional[int] = None) ->
         isNewEntry = doSql(dictDB, "insert into entries(id, doctype, xml, title, sortkey) values(?,?,?,?,?) on conflict(id) do nothing", (id, "", "", "", "")).rowcount > 0
 
     xml.attrs["lxnm:id"] = str(id)
-    del xml.attrs["lxnm:entryID"] # delete legacy ID
+    if "lxnm:entryID" in xml.attrs: # delete legacy id attribute
+        del xml.attrs["lxnm:entryID"]
     return id, isNewEntry
 
 def presave_subentries(dictDB: Connection, configs: Configs, entryXml: Tag, entryID: int, email: str):

website/test-import.py
@@ -483,6 +483,15 @@ testables = [{
             <text>title</text>
         </entry>
     """
+}, {
+    "description": "Test that legacy lxnm:entryID attribute is converted into new-style lxnm:id attribute",
+
+    "entry": """
+    <entry lxnm:entryID="201">some test</entry>
+    """,
+
+    "xml": """<entry lxnm:id="201" xmlns:lxnm="http://www.lexonomy.eu/">some test</entry>""",
+    "id": 201
 }]
 
 def overwriteConfig(originalConfig, overrides):